7bc3e46c516d1c2d1c27921ae828da90645e0858
jaumeortola <jaumeortola@gmail.com>
2015-04-19 19:35:04 +0200

    remove & in po

--- po2txtall.sh
+++ po2txtall.sh
@@ -3,7 +3,7 @@ for file in original/*.po
 do
     msgattrib --no-obsolete --no-fuzzy --translated $file > $file-filtrat.po
     po2txt $file-filtrat.po > $file.html
-    sed -i "s/_//g" $file.html
+    sed -i "s/[_&]//g" $file.html
     rm $file-filtrat.po
     rm $file
 done
