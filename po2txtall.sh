9a54877deb1ed2a061048cb4ac3dbbf17d8b93bb
jaumeortola <jaumeortola@gmail.com>
2015-04-19 23:31:28 +0200

    Elimina \n \t de fitxers po

--- po2txtall.sh
+++ po2txtall.sh
@@ -3,7 +3,8 @@ for file in original/*.po
 do
     msgattrib --no-obsolete --no-fuzzy --translated $file > $file-filtrat.po
     po2txt $file-filtrat.po > $file.html
-    sed -i "s/[_&]//g" $file.html
+    sed -i 's/[_&]//g' $file.html
+    sed -i 's/\\[rtn]/ /g' $file.html
     rm $file-filtrat.po
     rm $file
 done
